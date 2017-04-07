4fade8006ff2e13a2f5338376d391a8190e5d7bc
chandu01144 <chandrashekar>
2017-04-07 11:27:46 +0530

    test

---
+++ WebContent/chandutest/chandutest.controller.js
@@ -0,0 +1,38 @@
+sap.ui.controller("chandutest.chandutest", {
+
+/**
+* Called when a controller is instantiated and its View controls (if available) are already created.
+* Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
+* @memberOf chandutest.chandutest
+*/
+//	onInit: function() {
+//
+//	},
+
+/**
+* Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
+* (NOT before the first rendering! onInit() is used for that one!).
+* @memberOf chandutest.chandutest
+*/
+//	onBeforeRendering: function() {
+//
+//	},
+
+/**
+* Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
+* This hook is the same one that SAPUI5 controls get after being rendered.
+* @memberOf chandutest.chandutest
+*/
+//	onAfterRendering: function() {
+//
+//	},
+
+/**
+* Called when the Controller is destroyed. Use this one to free resources and finalize activities.
+* @memberOf chandutest.chandutest
+*/
+//	onExit: function() {
+//
+//	}
+
+});
